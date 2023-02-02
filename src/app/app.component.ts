54d2c1f8eb6ed4c1cee4055c982b168deddc7ff5
Dylan <dylan.reeder5@gmail.com>
2023-02-02 10:00:07 -0500

    assignment 2 -- refactor, content list, content card, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,10 @@
 import { Component } from '@angular/core';
-import { ContentList } from './helper-files/content-list';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './content-card/content-card.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'favorite-movies';
-
-  // temp: content card logic in root component
- contentList = new ContentList(); 
-
-  constructor() {
-    this.contentList.add({
-      id: 1,
-      title: 'Interstellar',
-      description: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.',
-      director: 'Christopher Nolan',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg',
-      type: 'Drama',
-      tags: ['prison', 'hope', 'freedom']
-    })
-    this.contentList.add({
-      id: 2,
-      title: 'The Godfather',
-      description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
-      director: 'Francis Ford Coppola',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg',
-      type: 'Crime',
-      tags: ['mafia', 'crime', 'family']
-    })
-    this.contentList.add({
-      id: 3,
-      title: 'The Dark Knight',
-      description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.',
-      director: 'Christopher Nolan',
-      imgURL: 'https://www.themoviedb.org/t/p/original/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
-      type: 'Action',
-      tags: ['crime', 'superhero', 'batman']
-    })
-  }
-  
-  renderContent() {
-    let content = '';
-    for (let i = 0; i < this.contentList.count(); i++) {
-      content += this.contentList.render(i);
-    }
-    return content;
-  }
 }
--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,52 +1,28 @@
-import { Component } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
+import { Component, Input } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
-  styleUrls: ['./content-card.component.css']
+  styleUrls: ['./content-card.component.css'],
 })
 export class ContentCardComponent {
- contentList = new ContentList(); 
- name = 'name';
+  @Input() content: Content | undefined;
+  /* 
+    Using an ngFor directive in your Content-list's html file, display 5 Content-card's by passing in the values from your Content array. 
+    Content-card's should be display the Content passed into it as folows: 
+      - Each Content-card tag has a 1 pixel border applied to it The title should be in a header tag The image, description, creator, type and tags are visible inside of the box. 
+      - Styles and positioning of these elements are your choice, so long as they're in the card. (Hint: remember that your tags is an array) Clicking on the image displays the Content's id and title in the console Add any necessary padding, and margins as you see fit 
+      - The first content card displayed should have a black solid border of 2 pixels around it.
+  */
 
+  // how can i bind this in here: <app-content-card [content]="content"></app-content-card>
   constructor() {
-    this.contentList.add({
-      id: 1,
-      title: 'Interstellar',
-      description: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.',
-      director: 'Christopher Nolan',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg',
-      type: 'Drama',
-      tags: ['prison', 'hope', 'freedom']
-    })
-    this.contentList.add({
-      id: 2,
-      title: 'The Godfather',
-      description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
-      director: 'Francis Ford Coppola',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg',
-      type: 'Crime',
-      tags: ['mafia', 'crime', 'family']
-    })
-    this.contentList.add({
-      id: 3,
-      title: 'The Dark Knight',
-      description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.',
-      director: 'Christopher Nolan',
-      imgURL: 'https://www.themoviedb.org/t/p/original/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
-      type: 'Action',
-      tags: ['crime', 'superhero', 'batman']
-    })
+    console.log(this.content);
   }
-  
-  renderContent() {
-    let content = '';
-    for (let i = 0; i < this.contentList.count(); i++) {
-      content += this.contentList.render(i);
-    }
-    return content;
+
+  onPress() {
+    console.log('button pressed');
+    console.log(this.content?.id, this.content?.title);
   }
-  // q: how can I use above method in html file?
-  // a: use interpolation {{ renderContent() }}
 }
